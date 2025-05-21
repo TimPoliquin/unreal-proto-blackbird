c0f0aeb00645218c2dd7fb9c141aa59b1295b694
Tim Poliquin <tim.poliquin@gmail.com>
2025-05-21 12:26:30 -0400

    Basic projectile

---
+++ Source/Blackbird/Private/AbilitySystem/Ability/BlackbirdProjectileActor.cpp
@@ -0,0 +1,37 @@
+﻿// Copyright Alien Shores 2025
+
+
+#include "AbilitySystem/Ability/BlackbirdProjectileActor.h"
+
+#include "Blackbird/Blackbird.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+// Sets default values
+ABlackbirdProjectileActor::ABlackbirdProjectileActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Component"));
+	CapsuleComponent->SetCollisionObjectType(ECC_Projectile);
+	CapsuleComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	CapsuleComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	CapsuleComponent->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+	CapsuleComponent->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+	CapsuleComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	CapsuleComponent->IgnoreActorWhenMoving(GetOwner(), true);
+	SetRootComponent(CapsuleComponent);
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Component"));
+	StaticMeshComponent->SetupAttachment(GetRootComponent());
+	StaticMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	StaticMeshComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	ProjectileComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Component"));
+}
+
+// Called when the game starts or when spawned
+void ABlackbirdProjectileActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
---
+++ Source/Blackbird/Public/AbilitySystem/Ability/BlackbirdProjectileActor.h
@@ -0,0 +1,29 @@
+﻿// Copyright Alien Shores 2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BlackbirdProjectileActor.generated.h"
+
+class UCapsuleComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class BLACKBIRD_API ABlackbirdProjectileActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABlackbirdProjectileActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Projectile")
+	TObjectPtr<UCapsuleComponent> CapsuleComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Projectile")
+	TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Projectile")
+	TObjectPtr<UProjectileMovementComponent> ProjectileComponent;
+};
